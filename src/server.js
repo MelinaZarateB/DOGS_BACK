65d0b3434cf56c188d29c7ea8053694fd1a5a7a6
Melina Zarate <melinazarate@Melinas-MBP.fibertel.com.ar>
2024-04-29 20:21:45 -0300

    ok

--- src/server.js
+++ src/server.js
@@ -16,6 +16,11 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors({
+    origin: 'https://dogs-front-bd5a.vercel.app',
+    credentials: true
+  }));
+  
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
