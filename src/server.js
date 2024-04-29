b1b4a53302e7d51a5403ac8d0d3e41b0774e175a
Melina Zarate <melinazarate@Melinas-MBP.fibertel.com.ar>
2024-04-29 20:17:22 -0300

    Nueva config de cors

--- src/server.js
+++ src/server.js
@@ -1,23 +1,44 @@
-
-/* Creacion del server */
-const express = require('express')
-const server = express();
-const { conn } = require('./db')
-const routes = require('./routes/index.js');
-
-const PORT = 3001;
-
-/* Configuracion del middleware - Comando: npm i morgan */
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
-server.use(morgan('dev'))
-/* Importacion y ejecucion de Cors - Comando: npm i cors */
+const routes = require('./routes/index.js');
 const cors = require('cors');
-server.use(cors('Access-Control-Allow-Origin'))
+const session = require('express-session');
 
-/* Middleware traductor que permite leer cuando llega data por req.body(generalmente metodo POST) */
-server.use(express.json())
+require('./db.js');
 
+const server = express();
+server.use(express.json());
+server.name = 'API';
+server.use(cors());
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Content-Security-Policy', "style-src 'self' https://www.gstatic.com 'unsafe-inline'");
+
+  next();
+});
+server.use(session({
+  secret: 'your-session-secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    sameSite: 'none',
+    secure: true
+  }
+}));
 server.use('/', routes);
 
-/* Se realiza la sincronizacion de sequelize con la DB y luego se levanta el servidor(si se sincronizo bien) */
+// Error catching endware.
+server.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ message: err.message || 'Internal server error' });
+});
 module.exports = server;
