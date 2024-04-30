214b217b2c337f6faed5a5d3e538a0efd6c398b8
Melina Zarate <melinazarate@Melinas-MBP.fibertel.com.ar>
2024-04-29 21:16:30 -0300

    ok4

--- src/server.js
+++ src/server.js
@@ -1,45 +1,16 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
-const cors = require('cors');
-const session = require('express-session');
-
-require('./db.js');
+const express = require("express");
+const router = ('./routes/index.js');
+const morgan = require("morgan");
+const cors = require("cors");
 
 const server = express();
+
+server.use(morgan("dev"));
 server.use(express.json());
-server.name = 'API';
 server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Content-Security-Policy', "style-src 'self' https://www.gstatic.com 'unsafe-inline'");
+server.use(router);
 
-  next();
-});
-server.use(session({
-  secret: 'your-session-secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    sameSite: 'none',
-    secure: true
-  }
-}));
-server.use('/', routes);
-
-// Error catching endware.
-server.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({ message: err.message || 'Internal server error' });
-});
 module.exports = server;
+
+
