0d7556c7e209908d6922c7031246eb89c3bbc1a6
Melina Zarate <melinazarate@Melinas-MBP.fibertel.com.ar>
2024-04-30 20:56:32 -0300

    config index

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* Conexion e iniciacion de la DB y el server */
 const server = require('./src/server.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001;
 
 /* Se realiza la sincronizacion de sequelize con la DB y luego se levanta el servidor(si se sincronizo bien) */
-conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('Server raised in port: 3001'); // eslint-disable-line no-console
-  });
-});
+conn.sync({ force: false }).then(() => {
+  server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server listening on port ${PORT}`);
+  })
+  }).catch(error => console.error(error))
