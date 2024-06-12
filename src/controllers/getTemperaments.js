5a32256991fbe887f0c381e4d72bd771a74c6188
Melina Zarate <melinazarate@Melinas-MBP.fibertel.com.ar>
2024-06-12 14:16:37 -0300

    Cambio en controller de temperaments

--- src/controllers/getTemperaments.js
+++ src/controllers/getTemperaments.js
@@ -6,7 +6,9 @@ const { allDogs } = require('./getAllDogs')
 
 const getTemperaments = async (req, res) => {
     try{
-        const { data } = await axios.get('https://api.thedogapi.com/v1/breeds?api_key=live_IZ6MS7Sbms9Zo0S2jnidv8oZmDKVtBrnVnmYfqQ9u7KyhylBRdpn9DzGwF3sSDGw')
+        //const { data } = await axios.get('https://api.thedogapi.com/v1/breeds?api_key=live_IZ6MS7Sbms9Zo0S2jnidv8oZmDKVtBrnVnmYfqQ9u7KyhylBRdpn9DzGwF3sSDGw')
+        const response = await fetch('https://api.thedogapi.com/v1/breeds?api_key=live_IZ6MS7Sbms9Zo0S2jnidv8oZmDKVtBrnVnmYfqQ9u7KyhylBRdpn9DzGwF3sSDGw')
+        const data = await response.json()
         let temperaments = [];
         data?.map((dog) => {
             if(dog.temperament){
